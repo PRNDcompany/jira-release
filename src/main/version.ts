79e22f3a0625eb0fe81374b683cdd18198626494
olaf-prnd <olaf@prnd.co.kr>
2023-02-21 17:11:53 +0900

    HDA-8216 fix: Version 클래스를 생성한다

---
+++ src/main/version.ts
@@ -0,0 +1,23 @@
+const versionRegExp = new RegExp(".*(\\d{1,5}\\.\\d{1,5}\\.\\d{1,5})", "g")
+
+export class Version {
+  major: number
+  minor: number
+  patch: number
+
+  constructor(major: number, minor: number, patch: number) {
+    this.major = major
+    this.minor = minor
+    this.patch = patch
+  }
+
+  public static from(text: string): Version | null {
+    const regexArray = versionRegExp.exec(text)
+    if (regexArray == null) return null
+
+    const rawVersion = regexArray[1]
+    const [major, minor, patch] = rawVersion.split(".")
+      .map(string => parseInt(string))
+    return new Version(major, minor, patch)
+  }
+}
---
+++ src/test/version.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "@jest/globals"
+import { Version } from "../main/version"
+
+test("1.101.102 문자열로 Version을 만들 수 있다", () => {
+  // when
+  const actual = Version.from("1.101.102")
+
+  // then
+  const expected = new Version(1, 101, 102);
+  expect(actual).toEqual(expected)
+})
